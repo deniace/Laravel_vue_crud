9e7794f5e3bf60087d6f23676c54cc4e0fe5a5ed
Deni Supriyatna <denisupriyatna01@gmail.com>
2021-03-07 21:56:57 +0700

    update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,43 @@
-import './bootstrap';
-import Vue from 'vue';
-import App from './components/ExampleComponent';
+require('./bootstrap');
+window.Vue = require('vue');
 
-const vm = new Vue({
-    el: '#app',
-    render: h => h(App)
-});
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import App from './components/App.vue';
+import SweetAlert2 from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+// const vm = new Vue({
+//     el: '#app',
+//     render: h => h(App)
+// });
+
+Vue.use(VueRouter);
+Vue.use(SweetAlert2);
+Vue.use(VueAxios, axios);
+
+import ArticleIndex from './components/ArticleIndex.vue';
+import ArticleCreate from './components/ArticleCreate.vue';
+import ArticleShow from './components/ArticleShow';
+import ArticleEdit from './components/ArticleEdit';
+import { component } from 'vue/types/umd';
+
+const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: ArticleIndex
+    },{
+        name:'crate',
+        path:'/article/create',
+        component:ArticleCreate
+    },{
+        name:'edit',
+        path:'/article/edit/:id',
+        component: ArticleEdit
+    }
+];
+
+const router = new VueRouter({mode: 'history', routes: routes});
+const app = new Vue(Vue.util.extend({router}, App)).$mount('#app');
